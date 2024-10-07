e64b8b64415f58435207639066f3b290d0eb887a
baro <knabao7a7@gmail.com>
2024-10-07 11:49:36 +0700

    api list

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { GoogleOAuthProvider } from "@react-oauth/google";
-import { ENVIRONMENT } from "./constant/environment.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId={ENVIRONMENT.REACT_APP_GOOGLE_AUTH_CLIENT_ID}>
-      <App />
-    </GoogleOAuthProvider>
+    <App />
   </React.StrictMode>
 );
